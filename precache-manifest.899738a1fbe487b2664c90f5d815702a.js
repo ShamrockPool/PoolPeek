659c105ca795c2c91abb518a1c1198144039f9c6
paddy blake <paddyblake09@hotmail.com>
2021-01-31 12:52:53 +0000

    Updates

---
+++ precache-manifest.899738a1fbe487b2664c90f5d815702a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "59c2998e80b5a44f4548",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/js/0.76962fa0.chunk.js"
+  },
+  {
+    "revision": "589a5625a6eb26eb08c9",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/css/main.d2be7167.chunk.css"
+  },
+  {
+    "revision": "589a5625a6eb26eb08c9",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/js/main.97e7be1d.chunk.js"
+  },
+  {
+    "revision": "f2e986166f72b3ab29b8",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/js/runtime~main.631cd191.js"
+  },
+  {
+    "revision": "be951f9e72ac7116e6ca",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/js/3.ce445cef.chunk.js"
+  },
+  {
+    "revision": "ee18a13dc317b9ea9e0e",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/js/4.c9a9a7e9.chunk.js"
+  },
+  {
+    "revision": "623c6123184476530658",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/css/5.eba30722.chunk.css"
+  },
+  {
+    "revision": "623c6123184476530658",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/js/5.4a43448f.chunk.js"
+  },
+  {
+    "revision": "cb6c9c35e8695af40c44",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/css/6.eba30722.chunk.css"
+  },
+  {
+    "revision": "cb6c9c35e8695af40c44",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/js/6.3b90188c.chunk.js"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "9bf131f1fa4fe189b054752873322324",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/media/pooltool.9bf131f1.png_thumb"
+  },
+  {
+    "revision": "87f970a5e6d3b0ad4571471e5d4f4d09",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/media/poolpm.87f970a5.png_thumb"
+  },
+  {
+    "revision": "5d6576d88efa09f9e3ad5ce2ae2d79a6",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/static/media/adapools.5d6576d8.png_thumb"
+  },
+  {
+    "revision": "b7e3b5c8c3b90b53a469521497af5940",
+    "url": "https://shamrockpool.github.io/PoolPeek.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/PoolPeek.github.io/precache-manifest.0b993c86f434e588032ccc75568f1428.js"
+  "https://shamrockpool.github.io/PoolPeek.github.io/precache-manifest.899738a1fbe487b2664c90f5d815702a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/PoolPeek.github.io/index.html", {
+workbox.routing.registerNavigationRoute("https://shamrockpool.github.io/PoolPeek.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
