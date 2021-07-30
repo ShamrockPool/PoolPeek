017bdf436934692f9219a006cf3f4189aa9179df
paddy blake <paddyblake09@hotmail.com>
2021-07-30 16:47:27 +0100

    Updates

---
+++ precache-manifest.0b6ad78647c96b757261bea785bdbfb8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9844cecc8262b6aecf0a0dbe9475bd53",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2f1a6f4a09d511466eb",
+    "url": "./static/css/2.a4c7e7cf.chunk.css"
+  },
+  {
+    "revision": "0156c09bc78704db0aba",
+    "url": "./static/css/main.ee82a1a1.chunk.css"
+  },
+  {
+    "revision": "b2f1a6f4a09d511466eb",
+    "url": "./static/js/2.aa600886.chunk.js"
+  },
+  {
+    "revision": "0156c09bc78704db0aba",
+    "url": "./static/js/main.f9ab7d7a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "e5b5c181b63f27bb196a9452ad798048",
+    "url": "./static/media/apple_store.e5b5c181.png"
+  },
+  {
+    "revision": "dc7dc4b0c69b5e9dcfcfb8b8a8c11e71",
+    "url": "./static/media/brave.dc7dc4b0.jpg"
+  },
+  {
+    "revision": "39f3d95f968c33583807c24964c12e59",
+    "url": "./static/media/eagle.39f3d95f.png"
+  },
+  {
+    "revision": "2f065a208e3c0377c7aa593f72a7ecfd",
+    "url": "./static/media/google_plays.2f065a20.png"
+  },
+  {
+    "revision": "a6d90ea745d55f91b1934cd830a4a879",
+    "url": "./static/media/logo-144x144.a6d90ea7.png"
+  },
+  {
+    "revision": "f98b92d52709d5a212ebeec3fd5f7a6d",
+    "url": "./static/media/shamrock.f98b92d5.png"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "./static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "846b9d8e350da22e6d13d42213a2fc6f",
+    "url": "./static/media/train.846b9d8e.png"
+  },
+  {
+    "revision": "5ced868744506fb0e1c3b71ace7b11f0",
+    "url": "./static/media/twitprofile.5ced8687.jpg"
+  }
+]);
