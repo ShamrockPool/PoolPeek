990febe9283c5470a03377de01fbebc1f1cb811f
paddy blake <paddyblake09@hotmail.com>
2022-02-16 15:44:48 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0575fb76ed0d024073d9f0f2e43871c6.js"
+  "/precache-manifest.51f7cad5f4d05f47ebe6df193ebc4d00.js"
 );
 
 self.addEventListener('message', (event) => {
